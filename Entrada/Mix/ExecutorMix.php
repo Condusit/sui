94d9c9163703edecf3d883189e97e070de626a12
Anderson <Anderson@192.168.1.184>
2017-09-18 09:44:12 -0300

    Adicionado intervalo de duas horas para cada chamada de Veiculos, Eventos e Motoristas para integração Mix

--- Entrada/Mix/ExecutorMix.php
+++ Entrada/Mix/ExecutorMix.php
@@ -12,17 +12,27 @@ use Entrada\Mix\Motorista;
 use Entrada\Mix\Posicao;
 use Entrada\Mix\EventoNotificacao;
 use Entrada\Mix\Construtor;
+use Apoio\Conexoes;
 //Carrega os uses necessário para execução das classes----
 
 //Executa o fluxo do programa-------------------------------------
 \Apoio\Helpers::msg("Integracao Mix", "Iniciando ciclo");
 
-$vei = new Veiculo();
-$vei->atualizarListaVeiculos();
-$eve = new Evento();
-$eve->atualizarListaEventos();
-$mot = new Motorista();
-$mot->atualizarListaMotorista();
+$connMix = Conexoes::conectarMix();
+$dataSecundaria = $connMix->query("select * from controle where controle.nome = 'secundarios'")->fetchAll();
+$dataComMais2Horas = date("Y-m-d H:i:s", strtotime("+2 hours", strtotime($dataSecundaria[0]['id'])));
+$data = date("Y-m-d H:i:s");
+if($data > $dataComMais2Horas){
+    $vei = new Veiculo();
+    $vei->atualizarListaVeiculos();
+    $eve = new Evento();
+    $eve->atualizarListaEventos();
+    $mot = new Motorista();
+    $mot->atualizarListaMotorista();
+    $connMix->query("update controle set controle.id = '{$data}' where controle.nome = 'secundarios'");
+}
+
+
 $pos = new Posicao();
 $pos->buscarPosicoes();
 $eveN = new EventoNotificacao();
